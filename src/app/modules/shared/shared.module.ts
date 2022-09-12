e24603d4b595744d1ad19810adfe78a5148e443c
Simon Josef Kreuzpointner <e12021358@student.tuwien.ac.at>
2022-09-12 18:23:11 +0200

    add icons

--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -6,6 +6,14 @@ import { PageLayoutComponent } from './components/page-layout/page-layout.compon
 import { SecondsPipe } from './pipes/seconds.pipe';
 import { FormErrorsComponent } from './components/form-errors/form-errors.component';
 import { FormErrorComponent } from './components/form-errors/form-error/form-error.component';
+import { exclamationDiamond, github, heartFill, NgxBootstrapIconsModule, questionCircle } from 'ngx-bootstrap-icons';
+
+const icons = {
+  heartFill,
+  github,
+  questionCircle,
+  exclamationDiamond,
+};
 
 const sharedDeclarations = [
   PageLayoutComponent,
@@ -19,11 +27,12 @@ const sharedImports = [
   FormsModule,
   ReactiveFormsModule,
   RouterModule,
+  NgxBootstrapIconsModule,
 ];
 
 @NgModule({
   declarations: [...sharedDeclarations],
-  imports: [...sharedImports],
+  imports: [...sharedImports, NgxBootstrapIconsModule.pick(icons)],
   exports: [...sharedImports, ...sharedDeclarations],
 })
 export class SharedModule {
